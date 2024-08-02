0ddf7537e1b05dca5c59aa5f14df60fab54f6919
SeungGuk-Ezllabs <jsg@ezllabs.co.kr>
2024-08-02 12:30:57 +0900

    자주 사용하는 함수

--- src/app/content/v1/serialziers.py
+++ src/app/content/v1/serialziers.py
@@ -7,7 +7,20 @@ class ContentSerializer(serializers.ModelSerializer):
     title = serializers.SerializerMethodField()
 
     def get_title(self, obj):
+        # self.context
         return "test : " + obj.title
+
+    def validate(self, attrs):
+        return attrs
+
+    def validate_title(self, data):
+        return data
+
+    def to_internal_value(self, data):
+        return super().to_internal_value(data)
+
+    def to_representation(self, instance):
+        return super().to_representation(instance)
 
     class Meta:
         model = Content
--- src/app/content/v1/views.py
+++ src/app/content/v1/views.py
@@ -14,3 +14,19 @@ class ContentViewSet(
     queryset = Content.objects.all()
     serializer_class = ContentSerializer
     permission_classes = []
+
+    def get_queryset(self):
+        print("get_queryset")
+        return super().get_queryset()
+
+    def get_serializer_context(self):
+        print("get_serializer_context")
+        return super().get_serializer_context()
+
+    def get_serializer(self, *args, **kwargs):
+        print("get_serializer")
+        return super().get_serializer(*args, **kwargs)
+
+    def get_object(self):
+        print("get_object")
+        return super().get_object()
