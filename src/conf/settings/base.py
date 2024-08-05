9090a3808f376d738ca2f746a7c6b27feae40e77
SeungGuk-Ezllabs <jsg@ezllabs.co.kr>
2024-08-05 11:15:27 +0900

    mod, 코드 준비

--- src/conf/settings/base.py
+++ src/conf/settings/base.py
@@ -37,7 +37,6 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
-    "allauth.account.middleware.AccountMiddleware",
     "whitenoise.middleware.WhiteNoiseMiddleware",
     "debug_toolbar.middleware.DebugToolbarMiddleware",
 ]
