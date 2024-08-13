f69caa827038bbde13effcb0dc89cafce3d8bec0
SeungGuk-Ezllabs <jsg@ezllabs.co.kr>
2024-08-13 09:41:32 +0900

    2024-08-13

--- src/conf/urls.py
+++ src/conf/urls.py
@@ -1,13 +1,21 @@
 from django.contrib import admin
+from django.http import HttpResponse
 from django.urls import path, include
 
 from app.content.v1.views import ContentView, ContentDetailView
 
 urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("404/", lambda request: HttpResponse("Not Found", status=404)),
+]
+
+urlpatterns += [
+    path("accounts/", include("allauth.urls")),
     path("content/", ContentView.as_view(), name="content"),
     path("content/<int:pk>/", ContentDetailView.as_view(), name="content_detail"),
-    path("admin/", admin.site.urls),
-    path("v1/", include("app.content.v1.urls")),
-    path("v1/", include("app.user.v1.urls")),
-    path("accounts/", include("allauth.urls")),
+]
+
+urlpatterns += [
+    path("v1/content/", include("app.content.v1.urls")),
+    path("v1/user/", include("app.user.v1.urls")),
 ]
